72c1db4a808e37d13fdc51e688b42a8f0f399b46
Paulo Aragão <33064367+paulocosmico@users.noreply.github.com>
2019-10-05 19:09:58 -0300

    inimigo recebendo item ao ser jogado por cima dele

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject item;
+    private float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        timer = 0.0f;
+        item = collision.GetComponent<GameObject>();
+        Invoke("ApplyItem",0.5f);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        timer = 1f;
+    }
+    private void ApplyItem()
+    {
+        if (timer < 0.9f) {
+            //o item foi aplicado
+            //TODO: APLICAR EFEITO VISUAL
+        }
+    }
+}
