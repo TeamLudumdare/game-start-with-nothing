a436a4d58bc2480c32f7119279f6e7e424d42c12
Paulo Aragão <33064367+paulocosmico@users.noreply.github.com>
2019-10-05 14:46:45 -0300

    script do player criado e campo iniciado

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int lifePoints;
+    private int id;//id referente ao seu indice no servidor
+    private int moves;//numero de movimentos que o jogador pode realizar em sua jogada
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
