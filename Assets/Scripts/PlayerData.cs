c51515b4dff74f30c9cca487c93949204db23581
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-05 20:23:52 -0300

    Melhorado os modelos de dados

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public class PlayerData
+{
+    public int lifePoints;
+    public string _id;
+    public string name;
+}
