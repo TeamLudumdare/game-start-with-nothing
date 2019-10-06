8ddabba51106379966098834da41ac062bcf6287
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-06 01:43:11 -0300

    Removendo a listagem do host do game

--- Assets/Scripts/SalaDeEspera/HUDSalaEspera.cs
+++ Assets/Scripts/SalaDeEspera/HUDSalaEspera.cs
@@ -9,14 +9,12 @@ public class HUDSalaEspera : MonoBehaviour
     [SerializeField] private Text aguardando;
     [SerializeField] private Text playersOnline;
     [SerializeField] private Text idLobby;
-    [SerializeField] private Text hostName;
 
     void Update()
     {
         nomeJogador.text = SocketController.Instance.Player.name;
-        aguardando.text = SocketController.Instance.Lobby.players < 4 ? "Waiting for players to enter ..." : "Waiting for host to start game";
+        aguardando.text = SocketController.Instance.Lobby.players < 4 ? "Waiting for players to enter ..." : "Anyone can press space to start game";
         playersOnline.text = SocketController.Instance.Lobby.players + " of 4 players entered";
         idLobby.text = "Lobby ID: " + SocketController.Instance.Lobby._id;
-        hostName.text = "Host: " + SocketController.Instance.Lobby.host.name;
     }
 }
