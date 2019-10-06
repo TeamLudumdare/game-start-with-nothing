7d65e9c285a33d65b511475e8d5697f1dafabfaa
Anderson Santos De Medeiros <anderson.medeiros97@imd.local>
2019-10-06 12:41:47 -0300

    Criado um SceneController

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    private static SceneController instance;
+    
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(instance);
+        }
+        DontDestroyOnLoad(instance);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private bool EnterLobby() {
+        var lobby = GameManager.Instance.Lobby;
+        return lobby != null && !Equals(SceneManager.GetActiveScene().name, "SalaDeEspera");
+    }
+
+    public static SceneController Instance {
+        get {
+            return instance;
+        }
+    }
+}
