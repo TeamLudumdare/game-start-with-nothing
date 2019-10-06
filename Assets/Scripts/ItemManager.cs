a5d047739979e52ab18b50574325549cf6fd5316
Paulo Aragão <33064367+paulocosmico@users.noreply.github.com>
2019-10-05 21:39:19 -0300

    corrigido a logica de drag dos items, agora ele verifica qual é seu alvo

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -14,19 +14,12 @@ public class ItemManager : MonoBehaviour
         if (gameObject.tag == "Chest") {
             BuyItem();
         }
-        else
-        {
-            DeleteItem();
-        }
     }
     private void BuyItem()
     {
         Instantiate(myPrefab, new Vector3(listItemsBar[numOfItems].transform.position.x, listItemsBar[numOfItems].transform.position.y,
                                           listItemsBar[numOfItems].transform.position.z -0.5f),Quaternion.identity);
         numOfItems++;
-    }
-    private void DeleteItem()
-    {
-
+        //TODO: ENVIAR PARA O SERVIDOR QUE FOI ADIONADO UM ITEM A SUA LISTA, PARA ELE ATUALIZAR PARA TODOS OS JOGADORES
     }
 }
